89fbb695f3e7dbf12cde121a0104177339358ff3
MarcusGrass <marcus.grass@protonmail.com>
2026-09-09 11:42:04 +0200

    docs: add more clear examples of what can go wrong, and how that can be handled

--- incapsula/data_source_abp_sites.go
+++ incapsula/data_source_abp_sites.go
@@ -40,8 +40,10 @@ func dataSourceAbpSites() *schema.Resource {
 			"look up a single Site by `site_id` or `name` instead.\n\n" +
 			"Note: The time of reading this datasource is not defined by the terraform module. That means that" +
 			"the read is subject to race-conditions. If a site is created or deleted in the same plan as this " +
-			"datasource is read, it may yield unexpected results. If deterministic operation is required, " +
-			"specify resource dependencies explicitly.",
+			"datasource is read, it may yield unexpected results, like an inexhaustive listing, which would cause " +
+			"e.g. `site_priority` to fail (as that requires an exhaustive list). " +
+			"If deterministic operation is required, specify resource dependencies explicitly to force terraform " +
+			"plan order, e.g. make `incapsula_abp_account_site_priority` depend on sites that you are creating.",
 
 		Schema: map[string]*schema.Schema{
 			"account_id": {
